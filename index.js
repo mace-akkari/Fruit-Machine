7c8c6888b791272073fbc0e867e74844f941fbb9
Patrick Orrell <orrell@gmail.com>
2020-06-30 20:16:50 +0100

    Implement credit check

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ credits.set(FAKEID, 10);
 
 
 app.get('/play', (req, res) => {
+  if(credits.get(FAKEID) <= 0) {
+    res.status(404)
+      .json({ error: "insufficient credits" });
+    return;
+  }
+
   const game = play();
   const { win, balance } = getResult(game, credits.get(FAKEID));
   credits.set(FAKEID, balance);
