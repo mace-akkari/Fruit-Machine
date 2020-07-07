90c7131c4c04cc11932700b25ba75e36928b1149
Patrick Orrell <orrell@gmail.com>
2020-07-07 20:23:43 +0100

    Update experiment code

--- experiment.js
+++ experiment.js
@@ -1,15 +1,39 @@
-import { isWinner, play } from './app.js';
+import { getResult, play } from './app.js';
 
 const N = 100000;
 
 let wins = 0;
 
 for (let i = 0; i < N; i++) {
-  const result = play();
-  if (isWinner(result)) {
+  const slots = play();
+  const balance = 1;
+  const result = getResult(slots, balance)
+  if(result.win) {
     wins++;
   }
 }
 
 console.log(`Number of games: ${N}, Number of wins: ${wins}`);
 console.log(`Ratio: ${((wins/N)* 100)}%`);
+
+
+function playTillBust() {
+  const HARD_LIMIT = 1000;
+  let balance = 10;
+  let plays = 0;
+  while(balance && plays !== HARD_LIMIT) {
+    const slots = play();
+    plays++;
+    const result = getResult(slots, balance);
+    balance = result.balance;
+  }
+  return plays;
+}
+
+const n = 1000;
+const results = [];
+for(let i = 0; i < n; i++) {
+  results.push(playTillBust());
+}
+const sum = results.reduce((a,c) => a + c ,0);
+console.log(sum/n)
