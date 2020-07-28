b23ce26548db1e1ad0f8e0cbcc58556380e5b9fa
Patrick Orrell <orrell@gmail.com>
2020-07-28 20:05:47 +0100

    Add some rules

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ function getRandomInt(max) {
         return Math.floor(Math.random() * max);
     }
 }
+
 function allElementsTheSame(arr) {
     const [head, ...tail] = arr;
     return tail.every((x) => head === x);
+}
+
+function arraysTheSame(arr0, arr1) {
+    return JSON.stringify(arr0) === JSON.stringify(arr1);
 }
 
 // Uses the getRandomInt function to get a number based on the Symbols array length 
@@ -21,18 +26,23 @@ export function play() {
     return slots;
 }
 
-function isWinner(slots) {
-    return allElementsTheSame(slots);
-}
-
-export function getResult(slots, balance) {
-    const win = isWinner(slots);
+function calculateNewBalance(slots, balance) {
     let newBalance;
-    if(win) {
+    if(allElementsTheSame(slots)) {
         newBalance = balance + 2;
+    } else if(arraysTheSame(slots.slice(0, 2), [0,0])) {
+        newBalance = balance + 1;
+    } else if(arraysTheSame(slots.slice(0, 2), [1,1])) {
+        newBalance = balance;
     } else {
         newBalance = balance - 1;
     }
+    return newBalance;
+}
+
+export function getResult(slots, balance) {
+    const newBalance = calculateNewBalance(slots, balance);
+    const win = newBalance >= balance;
     return {
         win,
         balance: newBalance
